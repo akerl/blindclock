98643d841268f62611ae5127dac2817036ae8dee
Les Aker <me@lesaker.org>
2023-05-30 09:09:19 -0400

    add custom font

--- main.go
+++ main.go
@@ -141,6 +141,7 @@ func main() {
 	router := gin.Default()
 	router.StaticFile("/favicon.ico", "./public/favicon.ico")
 	router.StaticFile("/", "./public/index.html")
+	router.StaticFile("/fonts/Roboto-Thin.ttf", "./public/fonts/Roboto-Thin.ttf")
 	router.GET("/state", getState)
 	router.POST("/state", postState)
 	router.POST("/pause", pause)
